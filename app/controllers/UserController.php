a66126aa01c4cf4f86c5c2beec108c8954571285
shams <shams@questionemploi.com>
2014-10-25 11:54:30 +0400

    work on the authentication login form with validation. authentication to follow later

---
+++ app/controllers/UserController.php
@@ -0,0 +1,59 @@
+<?php
+ 
+class UserController
+  extends Controller
+{
+  public function login()
+  {
+    if ($this->isPostRequest()) {
+      $validator = $this->getLoginValidator();
+  
+      if ($validator->passes()) {
+        $credentials = $this->getLoginCredentials();
+       
+        if (Auth::attempt($credentials)) {
+          die('here');
+          return Redirect::route("user/profile");
+        }
+        
+        return Redirect::back()->withErrors([
+          "password" => ["Credentials invalid."]
+        ]);
+      } else {
+        return Redirect::back()
+          ->withInput()
+          ->withErrors($validator);
+      }
+    }
+  
+    return View::make("user/login");
+  }
+  
+  protected function isPostRequest()
+  {
+    return Input::server("REQUEST_METHOD") == "POST";
+  }
+  
+  protected function getLoginValidator()
+  {
+    return Validator::make(Input::all(), [
+      "username" => "required",
+      "password" => "required"
+    ]);
+  }
+  
+  protected function getLoginCredentials()
+  {
+    return [
+      "username" => Input::get("username"),
+      "password" => Input::get("password")
+    ];
+  }
+
+
+    public function profile()
+    {
+      return View::make("user/profile");
+    }
+
+}
--- app/routes.php
+++ app/routes.php
@@ -1,48 +1,17 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the Closure to execute when that URI is requested.
-|
-*/
-
-Route::get('/', function()
-{
-    return View::make('hello');
-});
-
-/*
- * Authentication form
- */
-Route::get('login', function()
+/*Route::get('login', function()
 {
     return View::make('auth/login');
-});
+});*/
 
-Route::get('forgot_password', function()
-{
-    return View::make('auth/forgot_password');
-});
+//Tutorial
+Route::any("/", [
+ "as"   => "user/login",
+ "uses" => "UserController@login"
+]);
 
-<<<<<<< HEAD
-Route::get('password_reset', function()
-{
-    return View::make('auth/password_reset');
-});
-
-/*
- * Validate The Submitted CSRF Token
- */
-Route::post('register', array('before' => 'csrf', function()
-{
-    return 'You gave a valid CSRF token!';
-}));
-
-
-//change for fix purpose
-
+Route::any("/profile", [
+  "as"   => "user/profile",
+  "uses" => "UserController@profile"
+]);
